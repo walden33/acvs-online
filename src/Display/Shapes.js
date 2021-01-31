2ff78d702c7d7a3fadadc9636c316e50375b1b99
Walden Li <admin@waldenli.com>
2021-01-31 16:42:33 -0500

    Added class, id to most shape objects

--- src/Display/Shapes.js
+++ src/Display/Shapes.js
@@ -1,25 +1,37 @@
 /**
+ * A collection of classes that encapsulate shapes that can be added to a
+ * <DisplayDataset>.
+ * 
+ * @author Walden Y. Li
+ * @version 1.4 (01/31/2021)
+ * 
+ * @update 1.4 Added class, id to most shape objects
+ */
+
+/**
  * A class that represents an ACVS square. All the information, including its x
  * and y coordinates, side length (here denoted by w and h), color, digit on it,
  * whether it is a target square, whether it is an optimal target, etc.
- * 
- * 
  */
 disp.Rect = class {
     /**
      * 
-     * @param {String} x 
-     * @param {String} y 
-     * @param {String} w 
-     * @param {String} h 
-     * @param {String} fill : the color of the <rect>
+     * @param {string} x 
+     * @param {string} y 
+     * @param {string} w 
+     * @param {string} h 
+     * @param {string} fill : the color of the <rect>
+     * @param {string} className : class for the <rect> object
+     * @param {string} id : id for the <rect> object
      */
-    constructor(x, y, w, h, fill) {
+    constructor(x, y, w, h, fill, className=undefined, id=undefined) {
         this.x = x;
         this.y = y;
         this.width = w;
         this.height = h;
         this.fill = fill;
+        this.className = className;
+        this.id = id;
     }
 }
 
@@ -27,20 +39,25 @@ disp.Rect = class {
 disp.Circle = class {
     /**
      * 
-     * @param {String} cx
-     * @param {String} cy 
-     * @param {String} r 
-     * @param {String} fill 
-     * @param {String} stroke 
-     * @param {String} strokeWidth : <svg> <circle> attr "stroke-width" 
+     * @param {string} cx : center x
+     * @param {string} cy : center y
+     * @param {string} r : radius
+     * @param {string} fill : color
+     * @param {string} stroke : stroke color
+     * @param {string} strokeWidth : <svg> <circle> attr "stroke-width"
+     * @param {string} className : class for the <circle> object
+     * @param {string} id : id for the <circle> object
      */
-    constructor(cx, cy, r, fill, stroke, strokeWidth) {
+    constructor(cx, cy, r, fill, stroke, strokeWidth, className=undefined,
+        id=undefined) {
         this.cx = cx;
         this.cy = cy;
         this.r = r;
         this.fill = fill;
         this.stroke = stroke;
         this.strokeWidth = strokeWidth;
+        this.className = className;
+        this.id = id;
     }
 }
 
@@ -48,20 +65,25 @@ disp.Circle = class {
 disp.Line = class {
     /**
      * 
-     * @param {String} x1 
-     * @param {String} y1 
-     * @param {String} x2 
-     * @param {String} y2 
-     * @param {String} stroke 
-     * @param {String} strokeWidth : <svg> <line> attr "stroke-width" 
+     * @param {string} x1 
+     * @param {string} y1 
+     * @param {string} x2 
+     * @param {string} y2 
+     * @param {string} stroke 
+     * @param {string} strokeWidth : <svg> <line> attr "stroke-width" 
+     * @param {string} className : class for the <line> object
+     * @param {string} id : id for the <line> object
      */
-    constructor(x1, y1, x2, y2, stroke, strokeWidth) {
+    constructor(x1, y1, x2, y2, stroke, strokeWidth, className=undefined,
+        id=undefined) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
         this.stroke = stroke;
         this.strokeWidth = strokeWidth;
+        this.className = className;
+        this.id = id;
     }
 }
 
@@ -69,20 +91,24 @@ disp.Line = class {
 disp.Text = class {
     /**
      * 
-     * @param {String} text 
-     * @param {String} x 
-     * @param {String} y 
-     * @param {String} fill 
-     * @param {String} fontSize : <svg> -> <text> attr "font-size"
-     * @param {String} className : <svg> -> <text> attr "class"
+     * @param {string} text 
+     * @param {string} x 
+     * @param {string} y 
+     * @param {string} fill 
+     * @param {string} fontSize : <svg> -> <text> attr "font-size"
+     * @param {string} fontFamily : <svg> -> <text> attr "font-family"
+     * @param {string} className : class for the <text> object
+     * @param {string} id : id for the <text> object
      */
-    constructor(text, x, y, fill, fontSize, className=undefined, fontFamily=undefined) {
+    constructor(text, x, y, fill, fontSize, fontFamily=undefined,
+        className=undefined, id=undefined) {
         this.text = text;
         this.x = x;
         this.y = y;
         this.fill = fill;
         this.fontSize = fontSize;
-        this.className = className;
         this.fontFamily = fontFamily;
+        this.className = className;
+        this.id = id;
     }
 }
