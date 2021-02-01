a835119586fe22c53841367b2114e593bdeb5edf
Walden Li <admin@waldenli.com>
2021-02-01 16:23:29 -0500

    Changed utility class for workspace

--- src/Experiment/Steps/PrepStep/ConsentStep.js
+++ src/Experiment/Steps/PrepStep/ConsentStep.js
@@ -18,19 +18,19 @@ exp.ConsentStep = class extends(util.AbstractStep) {
         const session_id = util.Util.get_session_id();
 
         // The message
-        exp.HtmlGui.workspace().append("p")
+        util.Workspace.workspace().append("p")
             .html("The following is the consent form. Please read it carefully.")
             .style("font-size", "1.2em")
             .style("font-style", "italic")
             .style("text-align", "center")
 
         // Use an <iframe> to display the consent form
-        exp.HtmlGui.workspace().append("iframe")
+        util.Workspace.workspace().append("iframe")
             .attr("width", "70%")
             .attr("height", "400")
             .attr("src", CONSENT_FORM_URL);
         
-        const responseArea = exp.HtmlGui.workspace().append("div")
+        const responseArea = util.Workspace.workspace().append("div")
             .attr("id", "consent-response-area")
             .style("width", "80%")
             .style("display", "block")
@@ -41,19 +41,19 @@ exp.ConsentStep = class extends(util.AbstractStep) {
         .text("I agree to participate")
         .on("click", (function(){
             this._db.EventsTable.add_new_row("Subject agreed to consent form");
-            // alert("Before we get started, please answer 2 quick questions.");
-            // let age = prompt("Please type your age:", "N/A");
-            // let gender = prompt("Please type your gender:", "N/A");
+            alert("Before we get started, please answer 2 quick questions.");
+            let age = prompt("Please type your age:", "N/A");
+            let gender = prompt("Please type your gender (Female/Male/Non-Binary):", "N/A");
             alert("Thank you!");
             this._db._user_data = {
-                // self_reported_age: age,
-                // self_reported_gender: gender,
-                // sub_id: SUB_ID
+                self_reported_age: age,
+                self_reported_gender: gender,
+                // sub_id: SUB_ID,
                 prolific_id: prolific_id,
                 study_id: study_id,
                 session_id: session_id
             };
-            exp.HtmlGui.clear_workspace();
+            util.Workspace.clear_workspace();
             this.step_completed_signal.emit();
             }).bind(this));
 
@@ -61,9 +61,9 @@ exp.ConsentStep = class extends(util.AbstractStep) {
         .text("I do NOT agree to participate")
         .attr("class", "btn-wide")
         .on("click", () => {
-            exp.HtmlGui.clear_header();
-            exp.HtmlGui.clear_workspace();
-            exp.HtmlGui.append_paragraphs([
+            util.Workspace.clear_header();
+            util.Workspace.clear_workspace();
+            util.Workspace.append_paragraphs([
                 "<br><br><br><br>",
                 "You have declined to participate.",
                 "<br>",
