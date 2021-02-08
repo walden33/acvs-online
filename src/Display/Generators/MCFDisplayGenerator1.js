b244d9f6115b397f4f53a3e9f22b63c9b583b0db
Walden Li <admin@waldenli.com>
2021-02-08 14:09:28 -0500

    Created a kernal class for MCFDisplayGenerator

--- src/Display/Generators/MCFDisplayGenerator1.js
+++ src/Display/Generators/MCFDisplayGenerator1.js
@@ -2,53 +2,35 @@
  * The <DisplayGenerator> class for Mouse Click Foraging task.
  * 
  * @author Walden Y. Li
- * @version 1.0 (01/31/2021)
+ * @version 1.3 (02/08/2021)
+ * 
+ * @update 1.3 Made this class an extension of the base class
+ *  <MCFDisplayGenerator>.
+ * @update 1.2 Added a black object to the back of each display object to allow
+ *  some degree of deviation in mouse clicking.
  */
-disp.MCFDisplayGenerator = class {
+disp.MCFDisplayGenerator1 = class extends disp.MCFDisplayGenerator {
 
     constructor(n_trials, targ_sq_color, targ_cir_color) {
+
+        super();
+
         // Generator parameters
         this._n_total_trials = n_trials;
-
-        // Display settings
-        this._screen_x = 100;   // length of the main axis (horizontal)
-        this._screen_y = 80;    // length of the cross axis (vertical)
-        this._screen_x_border = 0;  // length of blank space on x axis from the left OR right border
-        this._screen_y_border = 5;  // length of blank space on y axis from the top OR bottom border
-        this._max_x_jitter = 3.5;
-        this._max_y_jitter = 3.5/1.25;
-
-        // Stimuli settings
-        this._n_items_x = 10;   // number of items on the main axis
-        this._n_items_y = 8;    // number of items on the cross axis
-        this._n_total_items = this._n_items_x * this._n_items_y;
-        this._n_targ_sq = 20;
-        this._n_targ_cir = 20;
-        this._n_dist_sq = 20;
-        this._n_dist_cir = 20;
         this._targ_sq_color = targ_sq_color;
         this._targ_cir_color = targ_cir_color;
         this._dist_sq_color = targ_cir_color;
         this._dist_cir_color = targ_sq_color;
-        this._circle_radius = 0.7;
-        this._square_size = 2;
 
         // An array of displays in the block this generator is responsible for
         this._block_displays = this._make_block_displays();
+
     }
 
-    _get_grid_pos() {
-        let result = new Map();
-        let i = 0;  // grid number, to be set as the keys of the output <Map>
-        let x = this._screen_x_border + ((this._screen_x - 2 * this._screen_x_border) / this._n_items_x) / 2;   // initialize x coord.
-        for (let j = 0; j < this._n_items_x; j++) {
-            let y = this._screen_y_border + ((this._screen_y - 2 * this._screen_y_border) / this._n_items_y) / 2;   // initialize y coord.
-            for (let k = 0; k < this._n_items_y; k++) {
-                result.set(i, [x, y]);
-                i++;
-                y += (this._screen_y - 2 * this._screen_y_border) / this._n_items_y;
-            }
-            x += (this._screen_x - 2 * this._screen_x_border) / this._n_items_x;  // inc. x coord. to the next col.
+    _make_block_displays() {
+        let result = [];
+        for (let i = 0; i < this._n_total_trials; i++) {
+            result.push(this._make_trial_display());
         }
         return result;
     }
@@ -140,25 +122,25 @@ disp.MCFDisplayGenerator = class {
         return result;
     }
 
-    _make_block_displays() {
-        let result = [];
-        for (let i = 0; i < this._n_total_trials; i++) {
-            result.push(this._make_trial_display());
-        }
-        return result;
-    }
+    // _make_block_displays() {
+    //     let result = [];
+    //     for (let i = 0; i < this._n_total_trials; i++) {
+    //         result.push(this._make_trial_display());
+    //     }
+    //     return result;
+    // }
 
 
-    /**
-     * "Public" method. Returns one <DisplayDataset> of this block if there is
-     * any left, returns null otherwise.
-     */
-    yield_trial_display() {
-        if (this._block_displays.length > 0) {
-            return this._block_displays.pop();
-        } else {
-            return null;
-        }
-    }
+    // /**
+    //  * "Public" method. Returns one <DisplayDataset> of this block if there is
+    //  * any left, returns null otherwise.
+    //  */
+    // yield_trial_display() {
+    //     if (this._block_displays.length > 0) {
+    //         return this._block_displays.pop();
+    //     } else {
+    //         return null;
+    //     }
+    // }
 
 }
---
+++ src/Display/Generators/MCFDisplayGeneratorKernel.js
@@ -0,0 +1,64 @@
+/**
+ * <MCFDisplayGenerator> is a base class for Mouse Click Foraging display
+ * generator classes. It contains basic settings and logic of a display.
+ * 
+ * @author Walden Y. Li
+ * @version 1.2 (02/08/2021)
+ */
+disp.MCFDisplayGenerator = class {
+
+    constructor() {
+
+        // Display settings
+        this._screen_x = 100;   // length of the main axis (horizontal)
+        this._screen_y = 80;    // length of the cross axis (vertical)
+        this._screen_x_border = 0;  // length of blank space on x axis from the left OR right border
+        this._screen_y_border = 5;  // length of blank space on y axis from the top OR bottom border
+        this._max_x_jitter = 3.5;
+        this._max_y_jitter = 3.5 / 1.25;
+
+        // Stimuli settings
+        this._n_items_x = 10;   // number of items on the main axis
+        this._n_items_y = 8;    // number of items on the cross axis
+        this._n_total_items = this._n_items_x * this._n_items_y;
+        this._n_targ_sq = 20;
+        this._n_targ_cir = 20;
+        this._n_dist_sq = 20;
+        this._n_dist_cir = 20;
+        this._circle_radius = 0.7;
+        this._square_size = 2;
+
+        // An array of all displays in this generator
+        this._block_displays = null;
+
+    }
+
+    _get_grid_pos() {
+        let result = new Map();
+        let i = 0;  // grid number, to be set as the keys of the output <Map>
+        let x = this._screen_x_border + ((this._screen_x - 2 * this._screen_x_border) / this._n_items_x) / 2;   // initialize x coord.
+        for (let j = 0; j < this._n_items_x; j++) {
+            let y = this._screen_y_border + ((this._screen_y - 2 * this._screen_y_border) / this._n_items_y) / 2;   // initialize y coord.
+            for (let k = 0; k < this._n_items_y; k++) {
+                result.set(i, [x, y]);
+                i++;
+                y += (this._screen_y - 2 * this._screen_y_border) / this._n_items_y;
+            }
+            x += (this._screen_x - 2 * this._screen_x_border) / this._n_items_x;  // inc. x coord. to the next col.
+        }
+        return result;
+    }
+
+    /**
+     * "Public" method. Returns one <DisplayDataset> of this block if there is
+     * any left, returns null otherwise.
+     */
+    yield_trial_display() {
+        if (this._block_displays.length > 0) {
+            return this._block_displays.pop();
+        } else {
+            return null;
+        }
+    }
+
+}
