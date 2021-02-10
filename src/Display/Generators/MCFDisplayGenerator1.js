3d41c0cdcf643dd196a5d94980400220318fbe5e
Walden Li <admin@waldenli.com>
2021-02-09 20:36:51 -0500

    Added background shape

--- src/Display/Generators/MCFDisplayGenerator1.js
+++ src/Display/Generators/MCFDisplayGenerator1.js
@@ -49,6 +49,7 @@ disp.MCFDisplayGenerator1 = class extends disp.MCFDisplayGenerator {
             // Set coordinates with a random jitter
             let x = grid[0] + (Math.random()-0.5) * this._max_x_jitter;
             let y = grid[1] + (Math.random()-0.5) * this._max_y_jitter;
+            // Add target shape
             result.add_a_rect(new disp.Rect(
                 x - this._square_size/2 + '',     // square anchor is top left corner, so center is x - sz/2
                 y - this._square_size/2 + '',
@@ -57,7 +58,17 @@ disp.MCFDisplayGenerator1 = class extends disp.MCFDisplayGenerator {
                 this._targ_sq_color,
                 "targ_sq_" + this._targ_sq_color,
                 "pos_" + String(grid_no)
-            ))
+            ));
+            // Add transparent ground shapes
+            result.add_a_rect(new disp.Rect(
+                x - this._background_rect_size/2 + '',     // square anchor is top left corner, so center is x - sz/2
+                y - this._background_rect_size/2 + '',
+                this._background_rect_size,
+                this._background_rect_size,
+                "transparent",
+                "targ_sq_" + this._targ_sq_color,
+                "pos_" + String(grid_no) + "_bg"
+            ));
         }
 
         // 2. Add distractor squares
@@ -76,7 +87,17 @@ disp.MCFDisplayGenerator1 = class extends disp.MCFDisplayGenerator {
                 this._dist_sq_color,
                 "dist_sq_" + this._dist_sq_color,
                 "pos_" + String(grid_no)
-            ))
+            ));
+            // Add transparent ground shapes
+            result.add_a_rect(new disp.Rect(
+                x - this._background_rect_size/2 + '',     // square anchor is top left corner, so center is x - sz/2
+                y - this._background_rect_size/2 + '',
+                this._background_rect_size,
+                this._background_rect_size,
+                "transparent",
+                "dist_sq_" + this._dist_sq_color,
+                "pos_" + String(grid_no) + "_bg"
+            ));
         }
 
         // 3. Add target circles
@@ -96,7 +117,17 @@ disp.MCFDisplayGenerator1 = class extends disp.MCFDisplayGenerator {
                 null,
                 "targ_cir_" + this._targ_cir_color,
                 "pos_" + String(grid_no)
-            ))
+            ));
+            // Add transparent ground shapes
+            result.add_a_rect(new disp.Rect(
+                x - this._background_rect_size/2 + '',     // square anchor is top left corner, so center is x - sz/2
+                y - this._background_rect_size/2 + '',
+                this._background_rect_size,
+                this._background_rect_size,
+                "transparent",
+                "targ_cir_" + this._targ_cir_color,
+                "pos_" + String(grid_no) + "_bg"
+            ));
         }
 
         // 4. Add distractor circles
@@ -116,31 +147,20 @@ disp.MCFDisplayGenerator1 = class extends disp.MCFDisplayGenerator {
                 null,
                 "dist_cir_" + this._dist_cir_color,
                 "pos_" + String(grid_no)
-            ))
-        }
+            ));
+            // Add transparent ground shapes
+            result.add_a_rect(new disp.Rect(
+                x - this._background_rect_size/2 + '',     // square anchor is top left corner, so center is x - sz/2
+                y - this._background_rect_size/2 + '',
+                this._background_rect_size,
+                this._background_rect_size,
+                "transparent",
+                "dist_cir_" + this._dist_cir_color,
+                "pos_" + String(grid_no) + "_bg"
+            ));
+        }        
 
         return result;
     }
-
-    // _make_block_displays() {
-    //     let result = [];
-    //     for (let i = 0; i < this._n_total_trials; i++) {
-    //         result.push(this._make_trial_display());
-    //     }
-    //     return result;
-    // }
-
-
-    // /**
-    //  * "Public" method. Returns one <DisplayDataset> of this block if there is
-    //  * any left, returns null otherwise.
-    //  */
-    // yield_trial_display() {
-    //     if (this._block_displays.length > 0) {
-    //         return this._block_displays.pop();
-    //     } else {
-    //         return null;
-    //     }
-    // }
 
 }
