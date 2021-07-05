abd794403eb8a6508bfa7b03572536698cd76daa
Walden Li <admin@waldenli.com>
2021-07-05 10:49:00 -0400

    Complete add diamond step

--- src/Display/Generators/ACFDisplayGenerator1.js
+++ src/Display/Generators/ACFDisplayGenerator1.js
@@ -75,7 +75,13 @@ disp.ACFDisplayGenerator1 = class extends disp.ACFDisplayGenerator {
         util.Util.fisher_yates_shuffle(items);
 
         // Determine number of each item type in the display
-        this._set_item_count("red", "square", 100);
+        // this._set_item_count(this._targ_sq_color, "square", this._n_opt_color_1_targ);
+        // this._set_item_count(this._targ_cir_color, "circle", this._n_opt_color_2_targ);
+        // this._set_item_count(this._targ_diamond_color, "diamond", this._n_non_opt_color_targ);
+        this._set_item_count(this._targ_sq_color, "square", 25);
+        this._set_item_count(this._targ_cir_color, "circle", 25);
+        this._set_item_count(this._targ_diamond_color, "diamond", 50);
+
         
         // 1. Add squares
         this._colors.forEach( (function add_squares(color) {
@@ -148,6 +154,46 @@ disp.ACFDisplayGenerator1 = class extends disp.ACFDisplayGenerator {
                     this._colors_are_equal(color, this._targ_cir_color) ?
                         `targ_cir_${this._get_color_alias(color)}` :
                         `dist_cir_${this._get_color_alias(color)}`,
+                    `pos_${grid_no}_bg`
+                ));
+            }
+
+        }).bind(this) );
+
+        // 3. Add diamonds
+        this._colors.forEach( (function add_diamonds(color) {
+
+            for (let i = 0; i < this._get_item_count(color, "diamond"); i++) {
+                // Get grid info
+                let grid_no = items.pop();
+                let grid = gridPos.get(grid_no);
+                // Set coordinates with a random jitter
+                let x = grid[0] + (Math.random() - 0.5) * this._max_x_jitter;
+                let y = grid[1] + (Math.random() - 0.5) * this._max_y_jitter;
+                // Generate polygon
+                const poly = this._make_a_diamond(
+                    x,
+                    y,
+                    this._diamond_main_axis_len,
+                    this._diamond_cross_axis_len,
+                    this._get_color_value(color),
+                    this._colors_are_equal(color, this._targ_diamond_color) ?
+                        `targ_diamond_${this._get_color_alias(color)}` :
+                        `dist_diamond_${this._get_color_alias(color)}`,
+                    `pos_${grid_no}`
+                );
+                // Add target shape
+                result.add_a_polygon(poly);
+                // Add transparent ground shapes
+                result.add_a_rect(new disp.Rect(
+                    x - this._background_rect_size / 2 + '',     // square anchor is top left corner, so center is x - sz/2
+                    y - this._background_rect_size / 2 + '',
+                    this._background_rect_size,
+                    this._background_rect_size,
+                    "transparent",
+                    this._colors_are_equal(color, this._targ_diamond_color) ?
+                        `targ_diamond_${this._get_color_alias(color)}` :
+                        `dist_diamond_${this._get_color_alias(color)}`,
                     `pos_${grid_no}_bg`
                 ));
             }
