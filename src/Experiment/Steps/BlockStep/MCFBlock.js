2d0c47ba7c0bbcf3aa6878481acbbe40576c40ca
Walden Li <admin@waldenli.com>
2021-07-12 19:50:52 -0400

    Revert change

--- src/Experiment/Steps/BlockStep/MCFBlock.js
+++ src/Experiment/Steps/BlockStep/MCFBlock.js
@@ -2,11 +2,8 @@
  * <MCFBlock> represents a block of Mouse Click Foraging task.
  * 
  * @author Walden Y. Li
- * @version 1.2 (6/30/2021)
+ * @version 1.0 (1/31/2021)
  * 
- * @update 1.2 (6/30/21) Removed cb_no parameter in constructor. Trial needs to
- * read relevant information from logic of the display generator.
- * @created 1/31/2021
  */
 exp.MCFBlock = class extends util.AbstractStep {
 
@@ -15,11 +12,11 @@ exp.MCFBlock = class extends util.AbstractStep {
      * @param {util.Database} db 
      * @param {number} block_no 
      * @param {disp.DisplayGenerator} display_generator
-     * @param {number} n_max_trials : if display_generator has more displays than
+     * @param {number} cb_no : 1 or 0 
+     * @param {number} n_trials : if display_generator has more displays than
      *  desired, set the trial number needed here 
      */
-    constructor(db, block_no, display_generator, n_max_trials=undefined) {
-        
+    constructor(db, block_no, display_generator, cb_no=undefined, n_max_trials=undefined) {
         super();
 
         this._db = db;
@@ -35,15 +32,15 @@ exp.MCFBlock = class extends util.AbstractStep {
         display_generator instanceof disp.MCFBaselineDisplayGenerator ?
             this._block_type = "baseline" : this._block_type = "main";
 
-        // this._targ_sq_color = "";
-        // this._targ_cir_color = "";
-        // if (cb_no === 1) {
-        //     this._targ_sq_color = "red";
-        //     this._targ_cir_color = "green";
-        // } else if (cb_no === 0) {
-        //     this._targ_sq_color = "green";
-        //     this._targ_cir_color = "red";
-        // }
+        this._targ_sq_color = "";
+        this._targ_cir_color = "";
+        if (cb_no === 1) {
+            this._targ_sq_color = "red";
+            this._targ_cir_color = "green";
+        } else if (cb_no === 0) {
+            this._targ_sq_color = "green";
+            this._targ_cir_color = "red";
+        }
 
         if (util.Util.is_test_mode()) {
         }
@@ -57,9 +54,9 @@ exp.MCFBlock = class extends util.AbstractStep {
 
     }
 
-    _construct_trial(stimuli, logic) {
+    _construct_trial(stimuli) {
         if (this._block_type === "main") {
-            return new exp.MCFTrial(stimuli, logic.targ_sq_color, logic.targ_cir_color, logic.opt_targ_color);
+            return new exp.MCFTrial(stimuli, this._targ_sq_color, this._targ_cir_color);
         } else if (this._block_type === "baseline") {
             return new exp.MCFBaselineTrial(stimuli);
         }
@@ -75,7 +72,7 @@ exp.MCFBlock = class extends util.AbstractStep {
             let display = this._display_generator.yield_trial_display();
             if (display !== null) {
                 // Create a new trial
-                let trial = this._construct_trial(display.stimuli, display.logic);
+                let trial = this._construct_trial(display);
                 trial.set_block_number(this._block_no);
                 trial.set_trial_number(this._trial_num);
                 this._trial_num++;
@@ -100,6 +97,7 @@ exp.MCFBlock = class extends util.AbstractStep {
 
     _save_data() {
         this._db.ExperimentTable.add_new_row(this._block_no, this._all_trials_data);
+        console.log(this._db);
     }
 
     _show_summary() {
