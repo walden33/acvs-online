1d9da487ea4166a279e9e3d232e64dd8b5d21522
Walden Li <admin@waldenli.com>
2021-07-15 09:38:02 -0400

    Update csv search criteria

--- analysis/ACF2/concat_demo.py
+++ analysis/ACF2/concat_demo.py
@@ -5,7 +5,8 @@ import pandas as pd
 def main():
     data_dir = 'Data/Demographic/'
     output_file = 'demographics.csv'
-    fnames = [data_dir + i for i in os.listdir(data_dir) if i.endswith('csv')]
+    fnames = [data_dir + i for i in os.listdir(
+        data_dir) if i.endswith('csv') and not i == 'demographics.csv']
     concated = pd.concat([pd.read_csv(f) for f in fnames])
     concated.to_csv(data_dir + output_file, index=False)
     print('Demographic csv files concatenated to ' + data_dir + output_file)
