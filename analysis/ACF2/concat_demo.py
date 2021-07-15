6e077547bd367a6f2089520b29a9c354f529a13b
Walden Li <admin@waldenli.com>
2021-07-14 22:39:59 -0400

    Initial commit

---
+++ analysis/ACF2/concat_demo.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+
+
+def main():
+    data_dir = 'Data/Demographic/'
+    output_file = 'demographics.csv'
+    fnames = [data_dir + i for i in os.listdir(data_dir) if i.endswith('csv')]
+    concated = pd.concat([pd.read_csv(f) for f in fnames])
+    concated.to_csv(data_dir + output_file, index=False)
+    print('Demographic csv files concatenated to ' + data_dir + output_file)
+
+
+if __name__ == "__main__":
+    main()
